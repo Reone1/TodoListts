15fc040cb0ed01de8b8573349881a4195c4f3e45
Re1one <j901207@gmail.com>
2021-02-23 22:04:38 +0900

    update: functional component

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,40 +2,21 @@ import * as React from "react";
 
 interface CounterProps {
   name: string;
-}
+};
 
-interface CounterState {
-  count: number;
-}
+const Counter: React.FC<CounterProps>  = ({name} : CounterProps) => {
+  const [count, setCount] = React.useState<number>(0);
 
-class Counter extends React.Component<CounterProps, CounterState> {
-  constructor(props: CounterProps) {
-    super(props);
-    this.state = {
-      count: 0
-    };
-  }
-
-  componentDidMount() {
-    setInterval(this.increase, 1000);
-  }
-
-  increase = () => {
-    const { count } = this.state;
-    this.setState({ count: count + 1 });
+  const increase = () => {
+    setCount(count + 1);
   };
 
-  render() {
-    const { name } = this.props;
-    const { count } = this.state;
-
-    return (
-      <React.Fragment>
-        <h1>{name} counter</h1>
-        <div>count value: {count}</div>
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      <h1 onClick={increase}>{name} counter</h1>
+      <div>count value: {count}</div>
+    </React.Fragment>
+  );
 }
 
 export default Counter;
